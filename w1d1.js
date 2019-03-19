702e40536779fe163ab730d036668e0bc9026af2
Simcord <mhecanthony@yahoo.com>
2019-03-19 15:37:01 -0400

    adding and avg

---
+++ w1d1.js
@@ -0,0 +1,21 @@
+// var a = 9;
+// var b = 11;
+// var z = a + b;
+// console.log(z);
+
+let a = 1;
+let b = 12;
+let c = 18;
+let d = 4;
+let e = 9;
+let f = 6;
+let g = 19;
+let h = 9;
+let i = 6;
+let j = 30;
+
+//let array = [1, 12, 18, 4, 9, 6, 19, 9, 6, 30];
+
+
+let sum1 = (a+b+c+d+e)/5;
+console.log(sum1);
