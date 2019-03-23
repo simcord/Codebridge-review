8df35e44bd7094ce8935b37ea0b3c5a5dba14d25
Simcord <mhecanthony@yahoo.com>
2019-03-22 21:32:06 -0400

    Update w1d1.js

--- w1d1.js
+++ w1d1.js
@@ -1,8 +1,12 @@
+// *******Tuesday March 19, 2019 @2pm  @ GA 2nd Floor*******
+
+// Adding
 // var a = 9;
 // var b = 11;
 // var z = a + b;
 // console.log(z);
 
+//************Add and divide to get AVG***********
 // let a = 1;
 // let b = 12;
 // let c = 18;
@@ -14,10 +18,8 @@
 // let i = 6;
 // let j = 30;
 
-
 // let avg1 = (a+b+c+d+e)/5;
 // console.log(sum1);
-
 
 // let a = 4;
 // let b = 10;
@@ -32,10 +34,12 @@
 
 
 // let avg1 = (a+b+c+d+e)/5;
+// console.log(avg1);
 
-
+// *******Unit 1.3 page 12; Nested If ELSE statements**************
 
 // let a = 149;
+
 // if(a < 100) {
 	
 // 	if(a > 0 ){
@@ -48,6 +52,8 @@
 // else{
 // 	console.log('Is greater then 100')
 // }
+
+//#########Unit 1.3 page 14;Else If Statements###############
 
 
 // let grade = 89;
@@ -65,20 +71,52 @@
 //  else if(grade >=55 ){
 //  	console.log('D');
 //  }
-
-//  else{
+//	else{
 //  	console.log('You got an F');
 //  }
+//*************HOMEWORK*****************
+
+// ************Unit 1.3 page 25;*****************
+//1. print 1,3,5,7 and 9.
+
+// for(let i=1; i<10; i=i+2) {
+// 	console.log('i= '+i+' is an odd number');
+// }
+
+// 2. print 2,4,6,8 and 10.
+
+// for(let i=2; i<=10; i=i+2) {
+// 	console.log('i= '+i+' is an even number');
+// }
+//********Unit 1.3 page 26;************
+//1. print create a loop that outputs multiples of 3. starting at 6. ending at 60.
+
+// for(let i=6; i<=60; i=i+3) {
+// 	console.log('i= '+i+' is a multiple of 3' )
+// }
+
+//2. looping a triangle (its missing the first hash)
+
+let word ="#";
+console.log(word);
+for(i=0; i<7; i++) {
+	word= word +"#";
+	console.log(word);
+}
 
 
-//##########Codesmith challenge for loops.###########
+
+
+
+
+//***************Codesmith challenge for loops.*****************
 // for(let i=10; i>0; i--){
 // 	console.log(i);
 // }
 
 
 
-//######Codesmith challenge for loops and arrays###########
+//**************Codesmith challenge for loops and arrays***********
 
 
 // const synonyms = ['fantastic', 'wonderful', 'great'];
@@ -96,7 +134,9 @@
 
 
 
-//######Codesmith challenge for loops and arrays indices###########
+//**************Codesmith challenge for loops and arrays indices***********
+
+
 
 
 
